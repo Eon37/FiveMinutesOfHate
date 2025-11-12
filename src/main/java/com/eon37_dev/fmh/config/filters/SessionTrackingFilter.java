50345cf1a61a0334d42a6b472fda50203bb474ee
Eon37 <shapiro.mariya@yandex.ru>
2025-11-12 23:55:48 +0500

    Change client tracking implementation in SessionTrackingFilter

--- src/main/java/com/eon37_dev/fmh/config/filters/SessionTrackingFilter.java
+++ src/main/java/com/eon37_dev/fmh/config/filters/SessionTrackingFilter.java
@@ -3,14 +3,14 @@ package com.eon37_dev.fmh.config.filters;
 import com.eon37_dev.fmh.utils.CookieUtils;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class SessionTrackingFilter implements Filter {
-  private static final ConcurrentHashMap<String, HttpSession> clientSession = new ConcurrentHashMap<>();
-  private static final ConcurrentHashMap<String, String> sessionClient = new ConcurrentHashMap<>();
+  private static final ConcurrentHashMap<String, Long> clientLastAccess = new ConcurrentHashMap<>();
 
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
@@ -19,40 +19,20 @@ public class SessionTrackingFilter implements Filter {
     String clientId = CookieUtils.getClientIdFromCookie(request);
 
     if (clientId != null) {
-      synchronized ("SESSION_TRACKING_FILTER_INCREMENT_SESSION_" + clientId) {
-        HttpSession session = request.getSession();
-        HttpSession oldSession = clientSession.get(clientId);
-
-        if (oldSession != null) {
-          if (!session.getId().equals(oldSession.getId())) {
-            oldSession.invalidate();
-            incrementSessionCount(session, clientId);
-          }
-        } else {
-          incrementSessionCount(session, clientId);
-        }
-      }
+      clientLastAccess.put(clientId, System.nanoTime());
     }
+
     chain.doFilter(req, res);
   }
 
-  private static void incrementSessionCount(HttpSession session, String clientId) {
-    String sessionId = session.getId();
-    sessionClient.compute(sessionId, (k, v) -> {
-      clientSession.put(clientId, session);
-      return clientId;
-    });
-  }
-
-  public static void decrementSessionCount(String sessionId) {
-    sessionClient.compute(sessionId, (k, v) -> {
-      if (v != null) clientSession.remove(v);
-      return null;
-    });
-  }
-
   public static int getAllSessionCount() {
-    return clientSession.size();
+    return clientLastAccess.size();
+  }
+
+  @Scheduled(fixedRate = 5 * 60 * 1000) //every 5 minutes
+  public void cleanClients() {
+    long currTimestamp = System.nanoTime();
+    clientLastAccess.entrySet().removeIf(e -> TimeUnit.NANOSECONDS.toMinutes(currTimestamp - e.getValue()) >= 5);
   }
 }
 
